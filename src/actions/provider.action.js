bd26e143f554873560e2ffe22cf1d142485e097a
Enrique Arias <enric@Enrique-Ariass-iMac.local>
2016-06-06 23:20:39 +0200

    Created services

--- src/actions/provider.action.js
+++ src/actions/provider.action.js
@@ -1,7 +1,13 @@
-import _ from 'underscore';
-import yuiFetch from '../utils/yui-fetch.js'
 import * as Notifications from '../notifications/actions/notification.action'
-import {fetchTranslations} from './translation.action'
+import {
+  fetchMediaTypes, 
+  fetchCountries, 
+  fetchMediaTypesTranslations, 
+  fetchCommonTranslations
+} from '../services/itunes.service'
+import {
+  fetchTranslations
+} from './translation.action'
 import {
   REQUEST_PROVIDER_DATA,
   RECEIVE_PROVIDER_DATA,
@@ -29,55 +35,6 @@ function errorInitialData() {
     errorTitle: 'Terminal error',
     errorMessage: 'This is embarrasing but the app has crashed. Click the button to reload, in case this message is shown again, please let us know.'
   }
-}
-
-
-// Call functions
-function fetchMediaTypes() {
-  return yuiFetch('/data/media-types.json')
-    .then((r) => {
-      console.log(r)
-      return _.findWhere(r.results, {store: 'podcast'})
-    })
-}
-
-function fetchCountries() {
-  return yuiFetch('/data/countries.json')
-    .then(r => {
-      let countriesWithPodcasts = _.inject(r.results, function(memo, country) {
-        if (country.stores.podcast) {
-          // If the country icon link has no host, then add it
-          if (!country.flag_icon.match(/^(http|https):\/\//)) {
-            memo.push({ ...country, flag_icon: 'https://rss.itunes.apple.com' + country.flag_icon });
-          }
-          else {
-            memo.push(country);
-          }
-        }
-        return memo;
-      }, []);
-
-      // grouping by region
-      console.log(_.groupBy(countriesWithPodcasts, (country) => country.region))
-
-      //
-
-      return countriesWithPodcasts
-    })
-}
-
-function fetchMediaTypesTranslations(language) {
-  return yuiFetch('/data/lang/' + language + 'media-types.json')
-    .then(r => {
-      return r.results
-    })
-}
-
-function fetchCommonTranslations(language) {
-  return yuiFetch('/data/lang/' + language + 'common.json')
-    .then(r => {
-      return r.results
-    })
 }
 
 function fetchData(language = navigator.language) {
---
+++ src/services/itunes.service.js
@@ -0,0 +1,49 @@
+import yuiFetch from '../utils/yui-fetch.js'
+import _ from 'underscore';
+
+export function fetchMediaTypes() {
+  return yuiFetch('/data/media-types.json')
+    .then((r) => {
+      console.log(r)
+      return _.findWhere(r.results, {store: 'podcast'})
+    })
+}
+
+export function fetchCountries() {
+  return yuiFetch('/data/countries.json')
+    .then(r => {
+      let countriesWithPodcasts = _.inject(r.results, function(memo, country) {
+        if (country.stores.podcast) {
+          // If the country icon link has no host, then add it
+          if (!country.flag_icon.match(/^(http|https):\/\//)) {
+            memo.push({ ...country, flag_icon: 'https://rss.itunes.apple.com' + country.flag_icon });
+          }
+          else {
+            memo.push(country);
+          }
+        }
+        return memo;
+      }, []);
+
+      // grouping by region
+      console.log(_.groupBy(countriesWithPodcasts, (country) => country.region))
+
+      //
+
+      return countriesWithPodcasts
+    })
+}
+
+export function fetchMediaTypesTranslations(language) {
+  return yuiFetch('/data/lang/' + language + 'media-types.json')
+    .then(r => {
+      return r.results
+    })
+}
+
+export function fetchCommonTranslations(language) {
+  return yuiFetch('/data/lang/' + language + 'common.json')
+    .then(r => {
+      return r.results
+    })
+}
