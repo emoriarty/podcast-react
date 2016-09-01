77572ee37f0fc6233b5eae0df3dc7c3b97496538
Enrique Arias <enric@Enriques-MacBook-Pro.local>
2016-09-01 17:20:58 +0200

    Created list and list item components

--- src/components/pages/CountriesComponent.js
+++ src/components/pages/CountriesComponent.js
@@ -1,14 +1,39 @@
 'use strict';
 
-import React from 'react';
+import React, {PropTypes} from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as NotificationActions from '../../actions/notifications';
+import MdlList from '../lists/MdlListComponent';
+import MdlSwitchItem from '../lists/MdlSwitchItemComponent';
 
 require('styles/pages/Countries.sass');
 
 class CountriesComponent extends React.Component {
   render() {
+    let regions = this.props.provider.data.regions;
+    let regionTranslations = this.props.translations.data.app.regions;
+    let countryTranslations = this.props.translations.data.commons.feed_country;
     return (
       <div className="countries-component">
-        Please edit src/components/pages//CountriesComponent.js to update this component!
+        <MdlList>
+          {
+            regions.map(function(region, index) {
+              return (
+                <li>
+                  <h3>{regionTranslations[region.translation_key]}</h3>
+                  <MdlList>
+                    {
+                      region.countries.map(function(country, index) {
+                        return <MdlSwitchItem id={region.id + "-" + country.translation_key} key={index} title={countryTranslations[country.translation_key]}></MdlSwitchItem>
+                      })
+                    }
+                  </MdlList>
+                </li>
+              )
+            })
+          }
+        </MdlList>
       </div>
     );
   }
@@ -20,4 +45,25 @@ CountriesComponent.displayName = 'PagesCountriesComponent';
 // CountriesComponent.propTypes = {};
 // CountriesComponent.defaultProps = {};
 
-export default CountriesComponent;
+CountriesComponent.propTypes = {
+  actions: PropTypes.object.isRequired,
+  config: PropTypes.object.isRequired,
+  translations: PropTypes.object.isRequired,
+  provider: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+  const { config, translations, provider } = state;
+  return { config, translations, provider };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      ...bindActionCreators(NotificationActions, dispatch)
+    },
+    dispatch
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CountriesComponent);
